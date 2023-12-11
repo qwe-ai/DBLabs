f27af4554dfbef3c5d286f44b318e7aeaca0283c
Екатерина <sokolova0ekaterina@gmail.com>
2023-12-11 12:10:10 +0300

    удален файл .py для лабы Neo4j

--- 3_Neo4j/neo.py
+++
@@ -1,6 +0,0 @@
-req = ""
-for i in range(1, 16):
-    req = req.join('Match ('+f'stop{i}'+': Stop {name: "'+f'stop{i}'+'"}), ('+f'stop{i+1}'+': Stop {name:"'+f'stop{i+1}'+'"})\n create ('+f'stop{i}'+')<-[:1]->('+f'stop{i+1}'+')\n')
-print(req)
-
-
